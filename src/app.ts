3dc96201569b1cae26adb04faeb3313decc97b51
Simen <simdevgen@outlook.com>
2023-03-30 14:16:57 +0200

    Added Project Input

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,25 @@
-// Code goes here!
 
-console.log('Initialization');
-console.log('Test');
+
+// PROJECT INPUT
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    constructor() {
+        // Making sure what I fetch will not be null (!) and of the HTMLTemplateElement type
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+        const importedNode = document.importNode(this.templateElement.content, true); // Global method provided
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();
+
+console.log('Hola');
